3ba574cfff144be4f678e09155eab5eb85be787c
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2021-12-30 00:32:07 -0800

    add auto sign in

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,37 @@
+import { useEffect, useState} from 'react';
+import { getSession } from 'next-auth/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
-
+import Loader from 'helpers/loader';
 import MainLayout from 'components/ui/layout.main';
-
-import { Provider } from 'react-redux';
+/// redux
+import { Provider, useDispatch } from 'react-redux';
 import store from 'store/index';
 import { createWrapper } from 'next-redux-wrapper';
+import { autoSignIn } from 'store/actions/user.action';
 
 function MyApp({ Component, pageProps }) {
+  const  [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    getSession().then( session => {
+      if(session){
+        dispatch(autoSignIn(session))
+      }
+      setLoading(false)
+    })
+  },[])
+
   return (
     <Provider store={store}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      { loading  ?
+          <Loader full={true}/>
+          :
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        }
     </Provider>
   )
 }
