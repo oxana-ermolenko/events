5e3e832f58dda00d2d4f482b074a6185431338d0
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2022-01-03 23:43:56 -0800

    add config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
+let BASE_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.PROD_URL;
+
+const nextConfig = {
+    publicRuntimeConfig: {
+        base_url: BASE_URL
+
+    }
+}
+
 module.exports = (phase,{ defaultConfig }) => {
     if(phase === PHASE_DEVELOPMENT_SERVER){
         return {
